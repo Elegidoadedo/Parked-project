b4c7202e25eeeb951bd5039c7db828b446b47c9f
elegidoadedo <jmoralesmnz@gmail.com>
2018-10-02 16:57:13 +0200

    fila 1 done

--- js/game.js
+++ js/game.js
@@ -38,15 +38,19 @@ Game.prototype._startLoop = function (){
   self.score = 100000;
   self.player = new Player(self.canvasElement);
   self.obs1 = new Obstacle(self.canvasElement, 135, 52, false);
-  self.obs2 = new Obstacle(self.canvasElement, 15, 118, false);
-  self.obs3 = new Obstacle(self.canvasElement, 15, 80, false);
+  self.obs2 = new Obstacle(self.canvasElement, 11, 108, false);
+  self.obs3 = new Obstacle(self.canvasElement, 11, 80, false);
   self.obs4 = new Obstacle(self.canvasElement, 140 , 20, false);
   self.finishZone = new Obstacle(self.canvasElement, 240, 6, true);
+  //LINEAS DE LA FILA 1
   self.line1 = new Line(self.canvasElement, 0, 39, 60, 3);
   self.line1b = new Line(self.canvasElement, 60, 39, 3, 6);
   self.line2 = new Line(self.canvasElement, 0, 70, 60, 3);
   self.line2b = new Line(self.canvasElement, 58, 66, 3, 12);
-
+  self.line3 = new Line(self.canvasElement, 0, 100, 60, 3);
+  self.line3b = new Line(self.canvasElement, 58, 96, 3, 12);
+  self.line4 = new Line(self.canvasElement, 0, 130, 60, 3);
+  self.line4b = new Line(self.canvasElement, 58, 126, 3, 12);
   
   self.handleKeyDown = function (evt) {
     
@@ -112,6 +116,10 @@ Game.prototype._drawAll = function () {
   self.line1b.draw();
   self.line2.draw();
   self.line2b.draw();
+  self.line3.draw();
+  self.line3b.draw();
+  self.line4.draw();
+  self.line4b.draw();
   self.obs1.draw();
   self.obs2.draw();
   self.obs3.draw();
